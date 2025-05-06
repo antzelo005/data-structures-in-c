179ef626bd3b207aaa0fb771d21cef9a4a80cb00
antzelo005 <128628117+antzelo005@users.noreply.github.com>
2025-05-06 21:19:16 +0300

    Update Queue main.c

--- MyQueue/main.c
+++ MyQueue/main.c
@@ -1,26 +1,39 @@
-#include "queue.h"
 #include <stdio.h>
+#include "queue.h"
 
-
-
-
-
-int main(){
-    printf("hello world\n");
+int main() {
     Queue q;
     Q_init(&q);
-    Q_enqueue(&q,8);
-    Q_enqueue(&q,4);
-    Q_enqueue(&q,3);
+
+    printf("Enqueue 8\n");
+    Q_enqueue(&q, 8);
+    printf("Enqueue 4\n");
+    Q_enqueue(&q, 4);
+    printf("Enqueue 3\n");
+    Q_enqueue(&q, 3);
+
+    printf("Current queue:\n");
     Q_print(q);
+
+    printf("Dequeue one element...\n");
     Q_dequeue(&q);
+
+    printf("Queue after dequeue:\n");
     Q_print(q);
 
+    // Προσθήκη περισσότερων στοιχείων (π.χ. γέμισμα)
+    for (int i = 10; i < 16; i++) {
+        Q_enqueue(&q, i);
+    }
 
+    printf("Queue after adding more elements:\n");
+    Q_print(q);
+
+    // Άδειασμα ουράς
+    while (!Q_isEmpty(q)) {
+        Q_dequeue(&q);
+        Q_print(q);
+    }
 
     return 0;
 }
-
-
-
-
